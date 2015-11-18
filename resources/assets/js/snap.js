f2da8c58ad5c4d8ce75346faf980ac1d986ccdc1
unknown <dbarnes3@N122W14.win.cis.ksu.edu>
2015-11-18 10:48:42 -0600

    Implemented Sprite drag/Drop Options

--- resources/assets/js/snap.js
+++ resources/assets/js/snap.js
@@ -1,11 +1,12 @@
 var s = Snap("#svgArea");
 
-var bigCircle = s.circle("30%","50%", "5%");
-bigCircle.attr({
-	fill: "green",
-	stroke: "#000",
-	strokeWidth:5
-});
+//for different screen sizes, shows edge of working area
+var maxX;
+var maxY;
+//current location of sprite
+var spritex = 100;
+var spritey = 100;
+
 var square = s.rect(0,0, "90%","89.75%");
 square.attr({
 	fill: "#FFFFFF",
@@ -13,3 +14,45 @@ square.attr({
 	stroke: "#000",
 	strokeWidth:1
 });
+
+var bigCircle = s.circle(spritex, spritey, "5%");
+bigCircle.attr({
+	fill: "green",
+	stroke: "#000",
+	strokeWidth:5
+});
+
+var start = function() {
+	maxX = square.node.width.baseVal.value;
+	maxY = square.node.height.baseVal.value;
+    this.ox = parseInt(this.attr("cx"));
+    this.oy = parseInt(this.attr("cy"));
+	spritex = this.ox;
+	spritey = this.oy;
+    console.log("Start move, ox=" + this.ox + ", oy=" + this.oy);
+}
+
+var move = function(dx, dy) {
+	this.attr({"cx": this.ox + dx, "cy": this.oy + dy});
+}
+
+var stop = function() {
+	
+	this.ox = parseInt(this.attr("cx"));
+	this.oy = parseInt(this.attr("cy"));
+	if(this.ox+this.node.r.animVal.value >maxX || this.oy +this.node.r.animVal.value>maxY)
+	{
+		this.ox = spritex;
+		this.oy = spritey;
+		this.attr({"cx": this.ox, "cy": this.oy});
+	}
+	if(this.ox-this.node.r.animVal.value <0 || this.oy-this.node.r.animVal.value <0)
+	{
+		this.ox = spritex;
+		this.oy = spritey;
+		this.attr({"cx": this.ox, "cy": this.oy});
+	}
+    console.log("Stop move, ox=" + this.ox + ", oy=" + this.oy);
+}
+
+bigCircle.drag(move, start, stop);
