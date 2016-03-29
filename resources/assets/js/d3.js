ce924b24c05ab17a21c10682c2d0540c05b54bc6
domhaeflinger <domhaeflinger@gmail.com>
2016-03-29 09:47:05 -0500

    Added attributes to d3.js

--- resources/assets/js/d3.js
+++ resources/assets/js/d3.js
@@ -4,15 +4,18 @@ var sprite = d3.select("#svgSprite")
 var mySquare= stage.append("rect")
   .attr("x",200)
   .attr("y",140)
-  .attr("width",20)
-  .attr("height",20)
+  .attr("width",40)
+  .attr("height",40)
 	.attr("id", 'c2')
 	.attr("rotationDegree", 0)
 	.attr("penDown", "false")
   .attr("strokeSize", 4)
 	.attr("strokePen", d3.rgb("#00ADEF"))
 	.attr("rotationStyle", "all")
-  .attr("pointDir", 0);
+  .attr("pointDir", 0)
+  .attr("fill", "purple")
+  .attr("stroke", "black")
+  .attr("stroke-width", 5);
 
 var	maxX = stage.node.width.baseVal.value; //TODO reset on resize
 var  maxY = stage.node.height.baseVal.value;
